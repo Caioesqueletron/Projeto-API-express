e6ba285b08bf573143c2f7bf67750725111a55b0
caioesqueletron <caiocaio620@gmail.com>
2020-12-22 23:12:01 -0300

    Colocando o delete

--- src/servidor.js
+++ src/servidor.js
@@ -12,11 +12,15 @@ app.get('/produtos',(req,res,next)=>{
 app.get('/produtos/:id',(req,res,next)=>{
     res.send(bancodeDados.getProduto(req.params.id))
 })
-app.post('/produtos',(req,res,next)=>{
+app.put('/produtos',(req,res,next)=>{
     const produto = bancodeDados.salvarProduto({
         nome: req.body.nome,
         preco: req.body.preco
     })
+    res.send(produto)//JSON
+})
+app.delete('/produtos/:id',(req,res,next)=>{
+    const produto = bancodeDados.excluirProduto(req.params.id)
     res.send(produto)//JSON
 })
 app.listen(porta,()=>{
